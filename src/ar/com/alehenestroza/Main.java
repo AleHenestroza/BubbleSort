2e1f27e470bbaf0c96cc71bbc1a13027f00b1ec6
Alejandro Henestroza <alehenestroza@gmail.com>
2022-01-18 20:31:21 -0300

    BubbleSort implemented

--- src/ar/com/alehenestroza/Main.java
+++ src/ar/com/alehenestroza/Main.java
@@ -1,9 +1,46 @@
 package ar.com.alehenestroza;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-    // write your code here
+        Random rand = new Random();
+        int[] numbers = new int[100000];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(10000);
+        }
+
+        System.out.println("Before:");
+        printArray(numbers);
+
+        bubbleSort(numbers);
+
+        System.out.println("\nAfter:");
+        printArray(numbers);
+    }
+
+    private static void bubbleSort(int[] numbers) {
+        boolean swapped;
+
+        do {
+            swapped = false;
+            for (int i = 0; i < numbers.length - 1; i++) {
+                if (numbers[i] > numbers[i + 1]) {
+                    swapped = true;
+                    int temp = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = temp;
+                }
+            }
+        } while (swapped);
+    }
+
+    private static void printArray(int[] numbers) {
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
     }
 
 }
